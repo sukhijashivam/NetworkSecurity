897eafdee21e5b65bedaf6870914a493039579f2
sukhijashivam <shivamsukhija002@gmail.com>
2025-08-04 22:43:59 +0530

    Completed running

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -26,8 +26,8 @@ from sklearn.ensemble import (
 import mlflow
 # from urllib.parse import urlparse
 
-# import dagshub
-# #dagshub.init(repo_owner='krishnaik06', repo_name='networksecurity', mlflow=True)
+import dagshub
+dagshub.init(repo_owner='sukhijashivam', repo_name='NetworkSecurity', mlflow=True)
 
 # os.environ["MLFLOW_TRACKING_URI"]="https://dagshub.com/krishnaik06/networksecurity.mlflow"
 # os.environ["MLFLOW_TRACKING_USERNAME"]="krishnaik06"
