131a8dc52f1abe42fa235f002f69b185e4d606b0
sukhijashivam <shivamsukhija002@gmail.com>
2025-08-05 18:34:12 +0530

    final render deploy

--- app.py
+++ app.py
@@ -102,7 +102,7 @@ from fastapi import FastAPI
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=10000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
 
 
 
